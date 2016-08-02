fb9d4b9467b4f6f6ecce6f266e7678485c5ad3ae
whizzer0 <whizzerZero@gmail.com>
2016-08-02 15:53:24 +0100

    Added images

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var getPixels = require("get-pixels")
  
-getPixels("labelscript.png", function(err, pixels) {
+getPixels("images/encoded.jpg", function(err, pixels) {
   if(err) {
     console.log("Bad image path")
     return
   }
-  console.log("got pixels", pixels.get(0,0,0))
+  console.log("got pixels", pixels.get(0,0,0), )
 })
