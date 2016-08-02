cb8b43ca26691db9ff5a4b94c505e77a57730acd
whizzer0 <whizzerZero@gmail.com>
2016-08-02 16:30:37 +0100

    Started developing image recognition

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+const COLUMNS = 8
+const ROWS = 4
+const SPACINGWIDTH = 190
+const SPACINGHEIGHT = 160
+const OFFSETWIDTH = 10
+const OFFSETHEIGHT = 25
+
 var getPixels = require("get-pixels")
- 
+
 getPixels("images/encoded.jpg", function(err, pixels) {
-  if(err) {
-    console.log("Bad image path")
-    return
-  }
-  console.log("got pixels", pixels.get(0,0,0), )
+	if(err) {
+		console.log("Bad image path")
+		return
+		}
+	console.log("pixel is", (withinLimit(pixels.get(570, 182, 0)) ? "black" : "white"))
+	scan(pixels)
 })
+
+function withinLimit(colourcode) {
+	if(colourcode > 128) {
+		return false
+	} else {
+		return true
+	}
+}
+
+function scan(pixels) {
+	var imageWidth = pixels.shape[0]
+	var imageHeight = pixels.shape[1]
+	console.log(imageWidth, imageHeight)
+}
